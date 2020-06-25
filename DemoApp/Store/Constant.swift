a06cd720e24c1c1c70b5384c473c793475f812fc
vivek bajirao deshmukh <deshmukh.v50@gmail.com>
2020-06-25 17:29:38 +0530

    Initial Commit

---
+++ DemoApp/Store/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//
+
+import Foundation
+
+enum Config {
+    // Copy base url here
+    static let baseUrl = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+}
+
